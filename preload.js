dcd57796d435593de6fca598bac9a21c80062f0d
Thomas Drebes <38798057+tdrebes@users.noreply.github.com>
2022-10-16 03:13:10 +0200

    Cleanup project

--- preload.js
+++ preload.js
@@ -1,12 +1,5 @@
-/**
- * The preload script runs before. It has access to web APIs
- * as well as Electron's renderer process modules and some
- * polyfilled Node.js functions.
- * 
- * https://www.electronjs.org/docs/latest/tutorial/sandbox
- */
- const NOTIFICATION_TITLE = 'Notification'
- const NOTIFICATION_BODY = 'This is the content of a notification.'
+const NOTIFICATION_TITLE = 'Notification'
+const NOTIFICATION_BODY = 'This is the content of a notification.'
 
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
